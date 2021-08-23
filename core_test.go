2e4f98ae8265eb35c534080b3734b15f7996125b
Michael Gerber <michael.gerber@betvictor.com>
2021-08-23 10:03:04 +0200

    Initial commit

---
+++ core_test.go
@@ -0,0 +1,48 @@
+package core
+
+import "testing"
+
+func TestTwoChild(t *testing.T) {
+
+	tests := []struct {
+		n int
+		c int
+	}{
+		{4, 16},
+		{16, 256},
+		{22, 88},
+		{34, 544},
+	}
+
+	for _, q := range tests {
+		r := TwoChild(q.n)
+		if r != q.c {
+			t.Errorf("TwoChild was incorrect, got: %d, want: %d.", q.n, q.c)
+		}
+	}
+
+}
+
+func TestThreeChild(t *testing.T) {
+
+	tests := []struct {
+		n int
+		c int
+	}{
+		{4, 4},
+		{16, 40},
+		{22, 112},
+		{34, 22},
+		{40, 52},
+		{52, 34},
+		{58, 76},
+	}
+
+	for _, q := range tests {
+		r := ThreeChild(q.n)
+		if r != q.c {
+			t.Errorf("ThreeChild was incorrect, got: %d, want: %d.", q.n, q.c)
+		}
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module ztalloc
+
+go 1.16
